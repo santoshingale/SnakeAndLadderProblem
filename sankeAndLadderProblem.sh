9afc75bcbe0644e52d41630230419695f583d11e
santoshingale <santoshingale876@gmail.com>
2019-12-12 17:02:18 +0530

    Added: case for no move,ladder and snake

--- sankeAndLadderProblem.sh
+++ sankeAndLadderProblem.sh
@@ -2,5 +2,20 @@
 echo "Welcome to sanke and lader game"
 
 START_POSITION=0
+NO_MOVE=0
+LADDER=1
+SNAKE=2
+
+position=$START_POSITION
 
 die=$((RANDOM % 6 +1))
+checkMove=$(( RANDOM % 3 ))
+
+case $checkMove in
+	$NO_MOVE) 
+		echo "No move";;
+	$LADDER)
+		position=$(( $position + $die ));;
+	$SNAKE)
+		position=$(( $position - $die ));;
+esac
