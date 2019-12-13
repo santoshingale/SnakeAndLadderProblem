003c861de84ba4d40c275442d4d6fa7724fec22a
santoshingale <santoshingale876@gmail.com>
2019-12-13 17:07:27 +0530

    Added: Feature to continue play till reach winning position

--- sankeAndLadderProblem.sh
+++ sankeAndLadderProblem.sh
@@ -5,17 +5,24 @@ START_POSITION=0
 NO_MOVE=0
 LADDER=1
 SNAKE=2
+WINNING_POSITION=100
 
 position=$START_POSITION
 
-die=$((RANDOM % 6 +1))
-checkMove=$(( RANDOM % 3 ))
+while [ $position -le $WINNING_POSITION ]
+do
+	die=$((RANDOM % 6 +1))
+	checkMove=$(( RANDOM % 3 ))
 
-case $checkMove in
-	$NO_MOVE) 
-		echo "No move";;
-	$LADDER)
-		position=$(( $position + $die ));;
-	$SNAKE)
-		position=$(( $position - $die ));;
-esac
+	case $checkMove in
+		$NO_MOVE) 
+		$LADDER)
+			position=$(( $position + $die ));;
+		$SNAKE)
+			position=$(( $position - $die ))
+			if [ $position -lt 0 ]
+			then
+				position=$START_POSITION
+			fi;;
+	esac
+done
